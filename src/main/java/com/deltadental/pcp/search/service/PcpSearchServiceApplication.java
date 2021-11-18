16ef7d9f798b058a1185fec73e5b1c0354d634d2
Kishore Basireddy <KBasireddy@delta.org>
2021-11-18 11:02:04 -0500

    initial commit

---
+++ src/main/java/com/deltadental/pcp/search/service/PcpSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deltadental.pcp.search.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcpSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcpSearchServiceApplication.class, args);
+	}
+
+}
