ff82607d3ef55a389d65e497e56f25f703a2e6c6
Kishore Basireddy <KBasireddy@delta.org>
2021-11-23 20:17:34 -0500

    master code

--- src/main/java/com/deltadental/pcp/search/application/PcpSearchServiceApplication.java
+++ src/main/java/com/deltadental/pcp/search/application/PcpSearchServiceApplication.java
@@ -2,12 +2,59 @@ package com.deltadental.pcp.search.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = { "com.deltadental.pcp.search.*" })
+@EnableSwagger2
+@EnableAsync
 public class PcpSearchServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PcpSearchServiceApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.deltadental.pcp.search.controller")).build();
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages/pcp-search-service-error-messages");
+		return messageSource;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfig() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Primary
+	@Bean
+	ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return objectMapper;
 	}
 
 }
