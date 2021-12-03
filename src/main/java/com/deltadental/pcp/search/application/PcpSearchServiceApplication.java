2b0860d252fe6d7a8d9c821d3aabfa81ddd589a5
Surendra K Kalahasthi <skalahasthi2@delta.org>
2021-12-03 09:24:52 -0800

    fixed swagger

--- src/main/java/com/deltadental/pcp/search/application/PcpSearchServiceApplication.java
+++ src/main/java/com/deltadental/pcp/search/application/PcpSearchServiceApplication.java
@@ -1,9 +1,5 @@
 package com.deltadental.pcp.search.application;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -12,53 +8,15 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
-import com.deltadental.pcp.search.controller.PCPSearchServiceController;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.extern.slf4j.Slf4j;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.deltadental.pcp.search.*" })
 @EnableSwagger2
-@Slf4j
 public class PcpSearchServiceApplication {
-
-	@Value("${enable.access.token.filter}")
-	String accessTokenFilterEnableFlag;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(PcpSearchServiceApplication.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		log.info("enable.access.token.filter:{}", accessTokenFilterEnableFlag);
-	}
-	
-	@Bean
-	public Docket swaggerApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage(PCPSearchServiceController.class.getPackageName()))
-	            .build()
-				.apiInfo(apiInfo());
-	}
-	
-	private ApiInfo apiInfo() {
-        String description = "Delta Dental Ins -  PCP Search Service";
-        return new ApiInfoBuilder()
-                .title("REST API")
-                .description(description)
-                .version("1.0")
-                .build();
-    }
 
 //	@Bean
 //	public SaajSoapMessageFactory messageFactory() {
@@ -83,14 +41,14 @@ public class PcpSearchServiceApplication {
 	@Bean
 	ObjectMapper objectMapper() {
 		ObjectMapper objectMapper = new ObjectMapper();
-	    objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
-	    objectMapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false);
-	    objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, false);
-	    objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
-	    objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
-	    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	    objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-	    return objectMapper;
+		objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES, false);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, false);
+		objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true);
+		objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+		return objectMapper;
 	}
 
 }
---
+++ src/main/java/com/deltadental/pcp/search/config/SpringFoxConfig.java
@@ -0,0 +1,32 @@
+package com.deltadental.pcp.search.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {                                    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())  
+          .build().apiInfo(apiInfo());                                           
+    }
+    
+    private ApiInfo apiInfo() {
+        String description = "Delta Dental Ins -  PCP Search Service";
+        return new ApiInfoBuilder()
+                .title("REST API")
+                .description(description)
+                .version("1.0")
+                .build();
+    }
+}
