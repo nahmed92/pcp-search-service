6e6d023a7d70fb212d6554f162c5ea9ae5dcb678
Kishore Basireddy <KBasireddy@delta.org>
2022-05-04 19:19:34 -0400

    'application.propertis'

--- src/main/java/com/deltadental/pcp/search/controller/PCPSearchServiceController.java
+++ src/main/java/com/deltadental/pcp/search/controller/PCPSearchServiceController.java
@@ -7,14 +7,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.deltadental.pcp.search.constants.PCPSearchServiceConstants;
 import com.deltadental.pcp.search.domain.BPBLResolutionResponse;
@@ -35,7 +32,6 @@ import com.deltadental.pcp.search.service.ProvidersAuditService;
 import com.deltadental.platform.common.annotation.aop.MethodExecutionTime;
 import com.deltadental.platform.common.exception.ServiceError;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
