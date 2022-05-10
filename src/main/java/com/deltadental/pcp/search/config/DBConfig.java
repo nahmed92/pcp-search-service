3322461a2ec8cfde4df4e7562317a66fd5071abf
Kishore Basireddy <KBasireddy@delta.org>
2022-05-10 14:29:18 -0400

    code refactor

--- src/main/java/com/deltadental/pcp/search/config/DBConfig.java
+++ src/main/java/com/deltadental/pcp/search/config/DBConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = { "com.deltadental.pcp.search.repos" })
 @EntityScan(basePackages = { "com.deltadental.pcp.search.entities" })
+@EnableJpaAuditing
 public class DBConfig {
 
 	@Value("${spring.datasource.url}")
