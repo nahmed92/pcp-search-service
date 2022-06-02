76bb895bad7b66b8628dcaf03c816fda94d993f7
Veerendra Tiwari <vtiwari@delta.org>
2022-06-02 14:17:45 -0400

    added new test cases for FacilitySearchController

--- src/test/java/com/deltadental/pcp/search/controller/FacilitySearchControllerTest.java
+++ src/test/java/com/deltadental/pcp/search/controller/FacilitySearchControllerTest.java
@@ -1,2 +1,36 @@
-package com.deltadental.pcp.search.controller;public class FacilitySearchControllerTest {
+package com.deltadental.pcp.search.controller;
+
+import com.deltadental.pcp.search.domain.Facility;
+import com.deltadental.pcp.search.service.PCPSearchService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+public class FacilitySearchControllerTest {
+
+    @InjectMocks
+    FacilitySearchController controller;
+
+    @Mock
+    PCPSearchService mockPCPSearchService;
+
+    @Test
+    public void testFacilitySearch_success(){
+        String facilityId = "F001";
+        Facility response = Facility.builder().build();
+        Mockito.when(mockPCPSearchService.searchFacility(facilityId)).thenReturn(response);
+        ResponseEntity expectedResponse = controller.facilitySearch(facilityId);
+
+        Assertions.assertEquals(expectedResponse.getStatusCode(), HttpStatus.OK);
+
+    }
+
+
 }
